790feca24cc29c240427d41a0b284723211e0630
Luca Becker <luca.becker@me.com>
2014-05-10 10:10:23 +0200

    getting there. but it's far from being done.

--- static/js/view_app.js
+++ static/js/view_app.js
@@ -5,21 +5,37 @@ App.Data = Ember.Object.extend({
 });
 
 App.Router.map(function() {
-  this.resource("startlists");
-  this.resource("results");
-  this.resource("resultsDetails");
+	this.resource("race", function() {
+		this.resource("startlists");
+		this.resource("results");
+	});
+  //this.resource("startlists");
+  //this.resource("results");
+  //this.resource("resultsDetails");
   this.resource("news");
 });
 
 App.IndexRoute = Ember.Route.extend({
   beforeModel: function() {
-    //this.transitionTo('startlists');
+    this.transitionTo('startlists');
   }
+});
+
+App.RaceRoute = Ember.Route.extend({
+	model : function () {
+		var startlist = App.Data.create({
+			"dummy" : "element",
+			data : null
+		});
+		startlistOutside = startlist;
+		return startlist;
+	}
 });
 
 App.StartlistsRoute = Ember.Route.extend({
 	model : function () {
 		var startlist = App.Data.create({
+			"dummy" : "element",
 			data : null
 		});
 		startlistOutside = startlist;
@@ -39,9 +55,17 @@ var socket = io.connect();
 socket.emit('request', { "type" : "startlist", "race_id" : "0"});
 
 socket.on("request", function(data) {
+	console.log("setze neue daten");
 	startlistOutside.set("data", data);
 });
 
 Ember.Handlebars.helper('console', function(value) {
     console.log(value);
+});
+
+Ember.Handlebars.helper("everyOther", function (index, amount, scope) {
+    if ( ++index % amount) 
+        return scope.inverse(this);
+    else 
+        return scope.fn(this);
 });
